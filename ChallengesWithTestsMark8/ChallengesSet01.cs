e41e7b5d39d658e3c636a216c07ecde5ab1946ba
Kenya Bogans <kbogans@hotmail.com>
2021-03-26 18:04:02 -0500

    completed set one

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,45 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
+            //throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return Math.Min(number1, number2);
+           //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+            //throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if(string.IsNullOrEmpty(nameOfPerson))
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+            //throw new NotImplementedException();
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
+            //throw new NotImplementedException();
         }
     }
 }
